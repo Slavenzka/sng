cb586e4eb0129a643561fa1bc41f78c1a83050a7
Ivan <proekt.energetika@gmail.com>
2018-12-05 22:46:21 +0300

    Написал движок к игре

---
+++ source/js/game-zoom.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(function () {
+  let area = document.querySelector('.game__area');
+  let items = area.querySelectorAll('.game__item');
+  let comment = area.querySelector('.game__comment');
+  let marks = document.querySelectorAll('.game__mark');
+
+  items.forEach((item, index) => {
+    item.addEventListener('click', function () {
+      let question = item.querySelector('.game__question');
+      let answer = item.querySelector('.game__answer');
+
+      question.classList.add('game__question--fade');
+
+      answer.classList.add('game__answer--reveal');
+      if (comment.classList.contains('game__comment--hide')) {
+        comment.classList.remove('game__comment--hide');
+      }
+      comment.textContent = item.dataset.answer;
+
+      marks[index].querySelector('.game__check').checked = 'true';
+    });
+  });
+})();
